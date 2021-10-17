141faf18cc1185e574ba590d65dadfe53300700f
Artemische <lis.fox690@gmail.com>
2021-10-17 14:10:32 +0300

    Change readFile func name

--- utils/file.js
+++ utils/file.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 const fileUtils = {
-    readFile: async (filePath) => {
+    readFileAsync: async (filePath) => {
 
         return new Promise((resolve, reject) => {
             fs.readFile(filePath, "utf-8", (err, data) => {
